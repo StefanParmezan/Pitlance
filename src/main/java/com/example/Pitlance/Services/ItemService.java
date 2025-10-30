257485687d0bf0b0f9f511c23b4eac597cb73e95
stefanparmezan <syromatnikov26@gmail.com>
2025-10-30 09:34:48 +0500

    Push commit

--- src/main/java/com/example/Pitlance/Services/ItemService.java
+++ src/main/java/com/example/Pitlance/Services/ItemService.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ItemService {
-
+    //Variables
     private final ItemRepository itemRepository;
 
     private final SellerService sellerService;
